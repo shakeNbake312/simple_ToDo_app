82d5bb277b8d929d2eacf5472f4fee95c6284385
shakeNbake312 <89243174+shakeNbake312@users.noreply.github.com>
2025-06-07 00:04:13 -0600

    Update database.py
    Continued trying to fix SQLALCHEMY_DATABASE_URL =

--- 001-fastapi-backend/database.py
+++ 001-fastapi-backend/database.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #SQLALCHEMY_DATABASE_URL = f"postgresql://{os.environ['DATABASE_USER']}:@{os.environ['DATABASE_HOST']}/{os.environ['DATABASE_NAME']}"
-SQLALCHEMY_DATABASE_URL = f"postgresql://{os.environ['DATABASE_USER']}:{os.environ['DATABASE_PASSWORD']}@{os.environ['DATABASE_HOST']}:{os.environ['DATABASE_PORT']/{os.environ['DATABASE_NAME']}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{os.environ['DATABASE_USER']}:{os.environ['DATABASE_PASSWORD']}@{os.environ['DATABASE_HOST']}:{os.environ['DATABASE_PORT']}/{os.environ['DATABASE_NAME']}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
